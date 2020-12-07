7fef4a3ae777b6f40a5da39eeaaf22df368b5247
LtBeer <kaesteu@gmail.com>
2020-12-07 19:40:23 +0000

    day 3 PS

---
+++ day3/day3_part1.psm1
@@ -0,0 +1,34 @@
+﻿Set-StrictMode -Version Latest -ErrorAction Stop
+function Get-Trees {
+
+    [CmdletBinding()]
+    [OutputType([void])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('InputFile')]
+        [string]
+        $File
+    )
+
+    begin {
+        $TreeCount = 0
+        $Locator = 1
+    }
+
+    process {
+        foreach($line in [System.IO.File]::ReadLines($File)) {
+        $PatternLength = $line.Length
+
+        if (($line.Substring($Locator - 1,1) -match "#")) {
+            $TreeCount++
+        }
+
+        $Locator = $Locator + 3
+
+            if ($Locator -gt $PatternLength) {
+                $Locator = $Locator - $PatternLength
+            }
+        }
+        Write-Host $TreeCount
+    }
+}
