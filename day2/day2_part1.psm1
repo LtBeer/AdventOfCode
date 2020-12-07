0991199a50eabf26a586f25e4d054f333d7cf2a2
LtBeer <kaesteu@gmail.com>
2020-12-07 19:04:25 +0000

    quick and dirty day 2 in PowerShell

---
+++ day2/day2_part1.psm1
@@ -0,0 +1,36 @@
+﻿Set-StrictMode -Version Latest -ErrorAction Stop
+
+function Get-ValidPasswords {
+
+    [CmdletBinding()]
+    [OutputType([void])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('InputFile')]
+        [string]
+        $File
+    )
+
+    begin {
+        $count = 0
+    }
+
+    process {
+
+        foreach($line in [System.IO.File]::ReadLines($File)) {
+
+            $min = $line.Remove($line.IndexOf('-'))
+            $max = [regex]::Match($line,"-(.*?) ").Groups[1].Value
+            $letter =  [regex]::Match($line," (.*?):").Groups[1].Value
+            $password = [regex]::Match($line,"(?<=: ).*$").Groups[0].Value
+
+            $occurences = ([regex]::Matches($password, $letter)).count
+
+            if (($occurences -ge $min) -and ($occurences -le $max)) {
+                $count++
+            }
+        }
+        Write-Host $count
+    }
+
+}
---
+++ day2/day2_part2.psm1
@@ -0,0 +1,38 @@
+﻿Set-StrictMode -Version Latest -ErrorAction Stop
+
+function Get-ValidPasswords {
+
+    [CmdletBinding()]
+    [OutputType([void])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('InputFile')]
+        [string]
+        $File
+    )
+
+    begin {
+        $count = 0
+    }
+
+    process {
+
+        foreach($line in [System.IO.File]::ReadLines($File)) {
+
+            $firstPos = $line.Remove($line.IndexOf('-'))
+            $secondPos = [regex]::Match($line,"-(.*?) ").Groups[1].Value
+            $letter =  [regex]::Match($line," (.*?):").Groups[1].Value
+            $password = [regex]::Match($line,"(?<=: ).*$").Groups[0].Value
+
+            if (($password.Substring($firstPos - 1,1) -match $letter) -and ($password.Substring($secondPos - 1,1) -notmatch $letter)) {
+                $count++
+            }
+            elseif (($password.Substring($firstPos - 1,1) -notmatch $letter) -and ($password.Substring($secondPos - 1,1) -match $letter)) {
+                $count++
+            }
+
+        }
+        Write-Host $count
+    }
+
+}
