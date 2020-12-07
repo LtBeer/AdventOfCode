bf75e2252334c9fa0801d94906cf8e524afa90ae
LtBeer <kaesteu@gmail.com>
2020-12-07 20:05:16 +0000

    as dirty as it gets but otherwise my brain would explode

---
+++ day3/day3_part2.psm1
@@ -0,0 +1,125 @@
+﻿Set-StrictMode -Version Latest -ErrorAction Stop
+function Get-Trees {
+
+    [CmdletBinding()]
+    [OutputType([void])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('InputFile')]
+        [string]
+        $File
+    )
+
+    begin {
+        $TreeCount = 0
+        $Locator = 1
+    }
+
+    process {
+        # Right 1, down 1
+        foreach($line in [System.IO.File]::ReadLines($File)) {
+            $PatternLength = $line.Length
+
+            if (($line.Substring($Locator - 1,1) -match "#")) {
+                $TreeCount++
+            }
+
+            $Locator = $Locator + 1
+
+                if ($Locator -gt $PatternLength) {
+                    $Locator = $Locator - $PatternLength
+                }
+            }
+
+        $RunningCount = $TreeCount
+        $TreeCount = 0
+        $Locator = 1
+
+        # Right 3, down 1
+        foreach($line in [System.IO.File]::ReadLines($File)) {
+        $PatternLength = $line.Length
+
+        if (($line.Substring($Locator - 1,1) -match "#")) {
+            $TreeCount++
+        }
+
+        $Locator = $Locator + 3
+
+            if ($Locator -gt $PatternLength) {
+                $Locator = $Locator - $PatternLength
+            }
+        }
+
+        $RunningCount = $RunningCount * $TreeCount
+        $TreeCount = 0
+        $Locator = 1
+
+        # Right 5, down 1
+        foreach($line in [System.IO.File]::ReadLines($File)) {
+            $PatternLength = $line.Length
+
+            if (($line.Substring($Locator - 1,1) -match "#")) {
+                $TreeCount++
+            }
+
+            $Locator = $Locator + 5
+
+                if ($Locator -gt $PatternLength) {
+                    $Locator = $Locator - $PatternLength
+                }
+            }
+
+        $RunningCount = $RunningCount * $TreeCount
+        $TreeCount = 0
+        $Locator = 1
+
+        # Right 7, down 1
+        foreach($line in [System.IO.File]::ReadLines($File)) {
+            $PatternLength = $line.Length
+
+            if (($line.Substring($Locator - 1,1) -match "#")) {
+                $TreeCount++
+            }
+
+            $Locator = $Locator + 7
+
+                if ($Locator -gt $PatternLength) {
+                    $Locator = $Locator - $PatternLength
+                }
+            }
+
+        $RunningCount = $RunningCount * $TreeCount
+        $TreeCount = 0
+        $Locator = 1
+
+        # Right 1, down 2
+        $linecount = 1
+        foreach($line in [System.IO.File]::ReadLines($File)) {
+            $PatternLength = $line.Length
+
+            if ($linecount % 2 -eq 0) {
+                $linecount++
+                continue
+            }
+
+            if (($line.Substring($Locator - 1,1) -match "#")) {
+                $TreeCount++
+            }
+
+            $Locator = $Locator + 1
+
+                if ($Locator -gt $PatternLength) {
+                    $Locator = $Locator - $PatternLength
+                }
+            $linecount++
+            }
+
+            $RunningCount = $RunningCount * $TreeCount
+            $TreeCount = 0
+            $Locator = 1
+
+            Write-Host "Running count: $RunningCount"
+
+
+    }
+}
